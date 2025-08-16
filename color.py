af52a1804761fdf68386e69acb06d0d9713669e5
Auguste Deroubaix <90181545+agtdbx@users.noreply.github.com>
2025-08-16 13:17:38 +0200

    Make color name readable and patch brown color

--- color.py
+++ color.py
@@ -2,6 +2,13 @@ from PyQt6.QtWidgets import QPushButton
 
 from define import COLORS_RBG, COLORS_BASIC, COLORS_NAMES
 
+def rgbToStyleColor(text, color):
+    return f"{text} rgb({color[0]}, {color[1]}, {color[2]});"
+
+def invertRgb(color):
+    return (255 - color[0], 255 - color[1], 255 - color[2])
+
+
 class Color:
     def __init__(self, id, window):
         self.id = id
@@ -25,16 +32,17 @@ class Color:
     def setSelected(self, selected):
         self.selected = selected
         if self.id != 63:
-            r, g, b = self.color
-            styleBackground = f"background-color: rgb({r}, {g}, {b});"
+            styleBackground = rgbToStyleColor("background-color :", self.color)
         else:
             styleBackground = ""
 
         if self.selected:
             if self.basic:
-                styleBorder = "border: 2px solid cyan;border-radius: 4px;"
+                styleBorder = "border: 2px solid cyan;"
             else:
-                styleBorder = "border: 2px solid yellow;border-radius: 4px;"
+                styleBorder = "border: 2px solid yellow;"
         else:
-            styleBorder = "border: 2px solid gray;border-radius: 4px;"
-        self.button.setStyleSheet(styleBackground + styleBorder)
+            styleBorder = "border: 2px solid gray;"
+        styleBorder += "border-radius: 8px;"
+        styleQtoolType = rgbToStyleColor("color :", invertRgb(self.color))
+        self.button.setStyleSheet(styleBackground + styleBorder + styleQtoolType)
